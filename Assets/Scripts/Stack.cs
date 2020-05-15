e264c2a711c6b1bab067ddf83e927a1bc2955e65
greenfieldxd <monsterbig53@gmail.com>
2020-05-15 18:36:03 +0300

    more fix

--- Assets/Scripts/Stack.cs
+++ Assets/Scripts/Stack.cs
@@ -6,15 +6,25 @@ using UnityEngine.UI;
 
 public class Stack : MonoBehaviour
 {
-    private const float bound_size = 10f; 
+    private const float bound_size = 12f;
+    private const float stack_moving_speed = 3f;
+    private const float error_margin = 0.1f;
 
     public GameObject[] stack;
+    Vector2 stackBounds = new Vector2(bound_size, bound_size);
 
     int stackIndex;
     int scoreCount = 0;
+    int combo = 0;
 
     float tileTransition = 0f;
-    float tileSpeed = 2f;
+    float tileSpeed = 1.2f;
+    float secondaryPos;
+
+    bool movingOnX = true;
+
+    Vector3 desiredPos;
+    Vector3 lastTilePos;
 
 
     // Start is called before the first frame update
@@ -29,7 +39,14 @@ public class Stack : MonoBehaviour
             stackIndex = transform.childCount - 1;
     }
 
-    public void Click()
+    private void Update()
+    {
+        MoveTile();
+        //Move stack
+        transform.position = Vector3.Lerp(transform.position, desiredPos, stack_moving_speed * Time.deltaTime);
+    }
+
+    public void Click() //Click button on Scene
     {
         if (PlaceTile())
         {
@@ -46,28 +63,72 @@ public class Stack : MonoBehaviour
 
     private void MoveTile()
     {
+        tileTransition += Time.deltaTime * tileSpeed;
+        if (movingOnX)
+        {
+            //move on X
+            stack[stackIndex].transform.localPosition = new Vector3(Mathf.Sin(tileTransition) * bound_size, scoreCount, secondaryPos); 
+        }
+        else
+        {
+            //move on Z
+            stack[stackIndex].transform.localPosition = new Vector3(secondaryPos, scoreCount, Mathf.Sin(tileTransition) * bound_size);
 
+        }
     }
 
     private void SpawnTile()
     {
+        lastTilePos = stack[stackIndex].transform.localPosition;
         stackIndex--;
         if (stackIndex < 0)
         {
             stackIndex = transform.childCount - 1;
         }
 
+        desiredPos = (Vector3.down) * scoreCount;
         stack[stackIndex].transform.localPosition = new Vector3(0, scoreCount, 0);
     }
 
     private bool PlaceTile()
     {
+        //присваиваем позицию новому tile относительно того где был предыдущий
+        Transform transform = stack[stackIndex].transform;
+
+        if (movingOnX)
+        {
+            float deltaX = lastTilePos.x - transform.position.x;
+
+            if (Mathf.Abs(deltaX) > error_margin)
+            {
+                //cut the tile
+                combo = 0;
+                stackBounds.x = Mathf.Abs(deltaX);
+                if (stackBounds.x <= 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (movingOnX)
+        {
+            secondaryPos = transform.localPosition.x;
+
+        }
+        else
+        {
+            secondaryPos = transform.localPosition.z;
+        }
+
+        movingOnX = !movingOnX;
+
         return true;
     }
 
     private void GameOver()
     {
-
+        //Game over
     }
 
 
